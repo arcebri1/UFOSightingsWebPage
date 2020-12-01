fb17ed391144c7d98ce6921b0daa3d70f654a921
Lorena Arce <you@example.com>
2020-11-30 21:36:06 -0800

    search table working, need to clear it

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -15,48 +15,82 @@ let tbody = d3.select("tbody");
 
 // Create event handlers 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+form.on("submit", runEnter);
+
+
+// tableData.forEach((UFOs) => {
+//     // console.log(UFOs);
+//     let row = tbody.append("tr");
+//     Object.entries(UFOs).forEach(([key, value]) => {
+//         // console.log(key, value);
+//         let cell = row.append("td");
+//         cell.text(value);
+//     });
+// });
 
 // Complete the event handler function
-function runEnter(){
+function runEnter() {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    tableData.forEach((UFOs) => {
+
+    // Select the input element and get the raw HTML node
+    let inputElement = d3.select("#datetime");
+
+    console.log(inputElement);
+
+    // Get the value property of the input element
+    let inputValue = inputElement.property("value");
+
+    // console.log(inputElement);
+
+    console.log(inputValue);
+
+    let filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(filteredData);
+
+    // // remove any children
+    // tbody.html("");
+
+
+    if (filteredData.length === 0) {
+        tbody.text(`No such UFO sightings exist`)
+    }
+
+    else filteredData.forEach((UFOs) => {
             // console.log(UFOs);
             let row = tbody.append("tr");
-            Object.entries(UFOs).forEach(([key,value]) => {
+            Object.entries(UFOs).forEach(([key, value]) => {
                 // console.log(key, value);
                 let cell = row.append("td");
                 cell.text(value);
-            }); 
+            });
         });
+    
+    
+//     filteredData.forEach((UFOs) => {
+//         // console.log(UFOs);
+//         let row = tbody.append("tr");
+//         Object.entries(UFOs).forEach(([key, value]) => {
+//             // console.log(key, value);
+//             let cell = row.append("td");
+//             cell.text(value);
+//         });
+//     });
 
+//     else (filteredData.length === 0) {
+//         tbody.text(`No such UFO sightings exist`)
+//     }
 
-
-
-
-    // // Select the input element 
-    // let inputElement = d3.select("#datetime");
-
-    // // Get the value property of the input element
-    // let inputValue = inputElement.property("value");
-
-    // // console.log(inputElement);
-
-    // console.log(inputValue);
-
-    // let filteredData = data.filter(ufo => ufo.datetime === inputValue);
-
-    // console.log(filteredData);
-
+// }
     //  // Select the table
     // let table = d3.select(".table");
 
     // // append to the table
     // let tr = table.append("tr");
-  
+
     // let td = tr.append("td");
 
     // // let i = td.append(filteredData);
